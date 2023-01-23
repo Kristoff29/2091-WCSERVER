cc8ff444b13dc20824e871d31d9d752f4438161f
Kristoff29 <122844416+Kristoff29@users.noreply.github.com>
2023-01-23 12:16:31 +0800

    Delete prelims directory

--- prelims/app.js
+++
@@ -1,7 +0,0 @@
-var myLogModule = require('./utility/log.js');
-
-myLogModule.info('Node is running..');
-
-var myLogModule = require('./utility/log.js');
-
-myLogModule.warning('Warning node not found..');
