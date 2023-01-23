0fbc42e8fb87aca0fac282102c34ec5b31b9a3f9
Kristoff29 <122844416+Kristoff29@users.noreply.github.com>
2023-01-23 20:50:47 +0800

    Add files via upload
    Laboratory Activity 2: Adding Numbers

---
+++ Prelim-Lab-Activity-2/addNumbers/add.js
@@ -0,0 +1,7 @@
+function add(number1, number2) {
+    sum_num = number1 + number2;
+    return sum_num;
+  }
+  
+  module.exports = add;
+  
---
+++ Prelim-Lab-Activity-2/addNumbers/app.js
@@ -0,0 +1,5 @@
+var addition = require('./add.js');
+
+console.log();
+console.log('The sum of 3 + 2 is: ', +addition(2, 3));
+console.log();
