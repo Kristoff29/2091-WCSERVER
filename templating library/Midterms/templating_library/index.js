0cb7fd3a26ed34d67047d7cb5f19893f879534d3
Kristoff29 <122844416+Kristoff29@users.noreply.github.com>
2023-03-13 12:57:25 +0800

    Add files via upload

---
+++ templating library/Midterms/templating_library/index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'My Resume' });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+// serve static files
+
+app.use(express.static('public'));
