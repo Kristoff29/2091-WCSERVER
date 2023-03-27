9a31bd9df06f0d0bfd66bc597f2e6647e71026f1
Kristoff29 <122844416+Kristoff29@users.noreply.github.com>
2023-03-27 12:26:48 +0800

    Add files via upload

---
+++ finals/Vue-Demo-1/scripts/books.js
@@ -0,0 +1,17 @@
+const books = Vue.createApp({
+    data() {
+        return {
+            showBooks: true,
+            btitle: 'The Ledger',
+            bauthor: 'Kristoffer Lacson', 
+            year: 2008
+        }
+    },
+    methods: {
+        toggleShowBooks () {
+            this.showBooks = !this.showBooks
+        }
+    }
+}
+);
+books.mount('#books')
