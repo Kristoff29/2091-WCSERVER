2e2235e31b0a6db5688445fa3f09dc46f0b53533
Kristoff29 <122844416+Kristoff29@users.noreply.github.com>
2023-04-19 18:57:33 +0800

    Add files via upload

---
+++ vue-app-resume/src/components/Skills.vue
@@ -0,0 +1,39 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import EducationIcon from './icons/IconEducation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Skills</template>
+    Training/Certification/Honors
+    <ul style="list-style-type: square;">
+        <li>Model Student Award</li>
+        Republic Central Colleges <br>
+        2015, 2016, 2017, 2018, 2019
+        <li>Loyalty Award</li>
+        Republic Central Colleges <br>
+        2019
+    </ul>
+    <br>
+    Programming Languages
+    <ol style="list-style-type: square;">
+        <li>Javascript</li>
+        <li>Python</li>
+        <li>PHP</li>
+        <li>SQL</li>
+        <li>HTML</li>
+        <li>CSS</li>
+    </ol>
+
+  </WelcomeItem>
+  
+
+</template>
